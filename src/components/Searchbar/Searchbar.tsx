1be6868a92085ca251a529499ed73c52d288dfbd
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2023-10-02 15:40:33 +0200

    Börjat med sökfuntion

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,13 +1,41 @@
 import './Searchbar.scss'
 import { FaSearch } from "react-icons/fa"
+import data from '../../assets/movies.json'
+import { useState } from 'react';
 
-function Searchbar() {
+const Searchbar = () => {
+  const [searchTerm, setSearchTerm] = useState<any>('');
+  const [filteredData, setFilteredData] = useState<any>([]);
+
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+    filterData(value);
+    if(searchTerm === null ){
+      setFilteredData([])
+    }
+  };
+
+
+  const filterData = (searchTerm: any) => {
+    const filteredData = data.filter((item) =>
+      item.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredData(filteredData);
+  };
+
   return (
-    <div>
-        <div className="input-wrapper">
-                <FaSearch id="SearchIcon" />
-                <input placeholder="Sök..." />
-            </div>
+    <div className='Searchbar'>
+      <div className="input-wrapper">
+        <FaSearch id="SearchIcon" />
+        <input placeholder="Sök" value={searchTerm} onChange={handleInputChange} />
+      </div>
+      <ul>
+        {filteredData.map((item) => (
+          <li key={item.title}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
