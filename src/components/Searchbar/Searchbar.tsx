e09e7dccd3be0eeb78cbfcf180ad8471423b5e03
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2023-10-02 14:37:09 +0200

    Skapat inputfält

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
 import './Searchbar.scss'
+import { FaSearch } from "react-icons/fa"
 
 function Searchbar() {
   return (
-    <div>Searchbar</div>
+    <div>
+        <div className="input-wrapper">
+                <FaSearch id="SearchIcon" />
+                <input placeholder="Sök..." />
+            </div>
+    </div>
   )
 }
 
--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,13 +1,15 @@
 import './HomeView.scss';
 import Header from '../../components/Header/Header';
 import Trending from '../../components/Trending/Trending'
+import Searchbar from '../../components/Searchbar/Searchbar';
 
 function HomeView() {
   return (
     <div>
       <Header />
       <Trending />
-      </div>
+      <Searchbar/>
+    </div>
   )
 }
 
