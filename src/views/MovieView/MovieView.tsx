f9de1a1ccacad3599856345a5416ac30c8bda40b
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-04 14:04:10 +0200

    Skapat latest movie click funktion

--- src/views/MovieView/MovieView.tsx
+++ src/views/MovieView/MovieView.tsx
@@ -1,77 +1,87 @@
 import Header from '../../components/Header/Header';
-
 import './MovieView.scss';
+import { RootState } from '../../redux/store';
+import { useSelector } from 'react-redux';
+import allMoviesData from '../../assets/movies.json';
+import { useEffect, useState } from 'react';
 
 function MovieView() {
   const isFavorited: boolean = false;
+  const [movie, setMovie] = useState<MovieData | undefined>(undefined);
 
-  const testMovie = {
-    title: 'The Shawshank Redemption',
-    year: 1994,
-    rating: 'R',
-    actors: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
-    genre: 'Drama',
-    synopsis:
-      'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
-    thumbnail:
-      'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX380_CR0,4,380,562_.jpg',
-  };
+  const latestClickedMovie: string = useSelector(
+    (state: RootState) => state.lastestMovie.latestMovie
+  );
 
-  function addToFavories() {
-    // TODO: Lägga till film i favorites...
+  useEffect(() => {
+    function findMovie() {
+      const foundMovie = allMoviesData.find(
+        (movie) => movie.title === latestClickedMovie
+      );
+      setMovie(foundMovie);
+    }
+    findMovie();
+  }, [latestClickedMovie]);
+
+  function addToFavorites() {
+    // TODO: Add the movie to favorites...
   }
 
   function removeFromFavorites() {
-    //  TODO: Ta bort till film från favorites...
+    // TODO: Remove the movie from favorites...
   }
 
   return (
     <div className='movie-view'>
       <Header />
 
-      <section className='movie-view__card'>
-        <div className='movie-view__card__container'>
-          <div>
-            <img
-              className='movie-view__card__container__thumbnailIMG'
-              src={testMovie.thumbnail}
-              alt={testMovie.title}
-            />
+      {movie ? (
+        <section className='movie-view__card'>
+          <div className='movie-view__card__container'>
+            <div>
+              <img
+                className='movie-view__card__container__thumbnailIMG'
+                src={movie.thumbnail}
+                alt={movie.title}
+              />
+            </div>
+            <div className='movie-view__card__container__info'>
+              <h1 className='movie-view__card__container__title'>
+                {movie.title}
+              </h1>
+              <h3 className='movie-view__card__container__year'>
+                {movie.year}, {movie.genre}
+              </h3>
+              <h3 className='movie-view__card__container__actors'>
+                {movie.actors.join(', ')}
+              </h3>
+              <h3 className='movie-view__card__container__rating'>
+                {movie.rating}
+              </h3>
+            </div>
           </div>
-          <div className='movie-view__card__container__info'>
-            <h1 className='movie-view__card__container__title'>
-              {testMovie.title}
-            </h1>
-            <h3 className='movie-view__card__container__year'>
-              {testMovie.year}, {testMovie.genre}
-            </h3>
-            <h3 className='movie-view__card__container__actors'>
-              {testMovie.actors.join(', ')}
-            </h3>
-            <h3 className='movie-view__card__container__rating'>
-              {testMovie.rating}
-            </h3>
-          </div>
-        </div>
 
-        <p className='movie-view__synopsis'>{testMovie.synopsis}</p>
+          <p className='movie-view__synopsis'>{movie.synopsis}</p>
 
-        {isFavorited ? (
-          <button
-            className='movie-view__delete-button'
-            onClick={() => removeFromFavorites()}
-          >
-            Remove from Favorites
-          </button>
-        ) : (
-          <button
-            className='movie-view__add-button'
-            onClick={() => addToFavories()}
-          >
-            Add to Favorites
-          </button>
-        )}
-      </section>
+          {isFavorited ? (
+            <button
+              className='movie-view__delete-button'
+              onClick={() => removeFromFavorites()}
+            >
+              Remove from Favorites
+            </button>
+          ) : (
+            <button
+              className='movie-view__add-button'
+              onClick={() => addToFavorites()}
+            >
+              Add to Favorites
+            </button>
+          )}
+        </section>
+      ) : (
+        '' // Todo: Fixa någon snygg placeholder...
+      )}
     </div>
   );
 }
