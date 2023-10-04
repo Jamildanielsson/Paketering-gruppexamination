520260c71b21d4574a18f4a06d7875dc3bb0c733
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-04 11:41:44 +0200

    Korrigerat ej fungerande kod

--- src/views/MovieView/MovieView.tsx
+++ src/views/MovieView/MovieView.tsx
@@ -1,71 +1,79 @@
 import Header from '../../components/Header/Header';
-import { useState } from 'react';
 
 import './MovieView.scss';
 
 function MovieView() {
-  // detta är det jag ska göra testa olika filmer</text och fixa till UI
+  const isFavorited: boolean = false;
 
-    // Hur ska jag ta emot bookmarks så jag kan ändra det i sessionen?
-    const [isBookmarked, setIsBookmarked] = useState<boolean>(false);
+  const testMovie = {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    rating: 'R',
+    actors: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+    genre: 'Drama',
+    synopsis:
+      'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
+    thumbnail:
+      'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX380_CR0,4,380,562_.jpg',
+  };
 
-    const movieInfo =
-    {
-      title: "The Shawshank Redemption",
-      year: 1994,
-      rating: "R",
-      actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
-      genre: "Drama",
-      synopsis: "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
-      thumbnail: "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX380_CR0,4,380,562_.jpg"
-    }
+  function addToFavories() {
+    // TODO: Lägga till film i favorites...
+  }
 
-    const actors = movieInfo.actors.map(a => {
-        return a + ", ";
-    });
-
-    function addToBookmarks() {
-      // lägg till bookmarks
-    }
-
-    function removeFromBookmarks() {
-      // ta bort bookmarks
-    }
-
+  function removeFromFavorites() {
+    //  TODO: Ta bort till film från favorites...
+  }
 
   return (
-    <div className='movieView'>
+    <div className='movie-view'>
       <Header />
-      
-      <section className='card'>
-        <div className='container'>
+
+      <section className='movie-view__card'>
+        <div className='movie-view__card__container'>
           <div>
-            <img src={movieInfo.thumbnail} />
+            <img
+              className='movie-view__card__container__thumbnailIMG'
+              src={testMovie.thumbnail}
+              alt={testMovie.title}
+            />
           </div>
-          <div className='info'>
-            <h1 className='titel'>{movieInfo.title}</h1>
-            <h3 className='year'>{movieInfo.year}, {movieInfo.genre}</h3>
-            <h3 className='actors'>{actors}</h3>
-            <h3 className='rating'>{movieInfo.rating}</h3>  
+          <div className='movie-view__card__container__info'>
+            <h1 className='movie-view__card__container__title'>
+              {testMovie.title}
+            </h1>
+            <h3 className='movie-view__card__container__year'>
+              {testMovie.year}, {testMovie.genre}
+            </h3>
+            <h3 className='movie-view__card__container__actors'>
+              {testMovie.actors.join(', ')}
+            </h3>
+            <h3 className='movie-view__card__container__rating'>
+              {testMovie.rating}
+            </h3>
           </div>
-        </div>
-        <div className='synopsis'>
-          {movieInfo.synopsis}
         </div>
 
-        {isBookmarked? (
-          <button className='movieView__button' onClick={() => removeFromBookmarks()}>Remove from Bookmark</button>
+        <p className='movie-view__synopsis'>{testMovie.synopsis}</p>
+
+        {isFavorited ? (
+          <button
+            className='movie-view__delete-button'
+            onClick={() => removeFromFavorites()}
+          >
+            Remove from Favorites
+          </button>
         ) : (
-          <button className='movieView__button' onClick={() => addToBookmarks()}>Add to Bookmark</button>
+          <button
+            className='movie-view__add-button'
+            onClick={() => addToFavories()}
+          >
+            Add to Favorites
+          </button>
         )}
-        
-        
       </section>
     </div>
-  )
+  );
 }
-
-
-  
 
 export default MovieView;
