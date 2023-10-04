a5c19da5f715463b222bf5e1a875a7563ad8bb0d
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2023-10-04 10:21:16 +0200

    Fortsätter jobba med karusell

--- src/components/Recommended/Recommended.tsx
+++ src/components/Recommended/Recommended.tsx
@@ -1,42 +1,36 @@
 import './Recommended.scss'
-import { useState } from 'react'
 import Movies from '../../assets/movies.json'
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, EffectCoverflow, Autoplay } from 'swiper/modules'
+
+
+
 
 function Recommended() {
 
-    const [slide, setSlide] = useState<number>(0);
-    const isTrendingComp = Movies.filter((movie => movie.isTrending))
+    const isNotTrendingComp = Movies.filter((movie => !movie.isTrending))
 
-    console.log(isTrendingComp);
-    
+    console.log(isNotTrendingComp);
 
-    const nextSlide = () => {
-        setSlide(slide === isTrendingComp.length - 1 ? 0 : slide + 1)
-    }
+    const imgComp = isNotTrendingComp.map((movie, index) => {
+<SwiperSlide style={{backgroundImage: `url(${movie.thumbnail})`}} className='my-slider'>
 
-    const prevSlide = () => {
-        setSlide(slide === 0 ? isTrendingComp.length - 1 : slide - 1)
-    }
-
-
+</SwiperSlide>
+    })
 
 
     return (
+        <div className='recommended'>
 
-        <div className='carousel'>
-
-            <h1>Recommended:</h1>
-            <BsArrowLeftCircleFill className='arrow arrow-left' onClick={prevSlide} data-testid='arrow' />
-            {Movies.map((movie: MovieData, index) => {
-                return (
-                    <img key={index} className={slide === index ? "slide" : "slide slide-hidden"} src={movie.thumbnail} alt={'Movie-thumbnail: ' + index} />
-                )
+        <Swiper>
+            {isNotTrendingComp.map((movie, index) => { return (
+                <SwiperSlide>
+                    <img src={movie.thumbnail} alt="" />
+                </SwiperSlide> )
             })}
-            <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide} data-testid='arrow' />
-            <span className='indicators'>{Movies.map((_, index) => {
-                return (<button key={index} className={slide === index ? "indicator" : "indicator indicator-inactive"} onClick={() => setSlide(index)}></button>)
-            })}</span>
+        </Swiper>
+
         </div>
 
     )
