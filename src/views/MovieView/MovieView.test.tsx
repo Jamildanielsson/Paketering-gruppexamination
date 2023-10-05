86b3d595d6af8913470eab6ebed0db3e7d2a9f92
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2023-10-05 13:38:56 +0200

    Test för att visa rätt filmsida vid val av film

--- src/views/MovieView/MovieView.test.tsx
+++ src/views/MovieView/MovieView.test.tsx
@@ -1,22 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
-import { BrowserRouter } from 'react-router-dom';
-import MovieView from './MovieView';
 import { Provider } from 'react-redux';
 import store from '../../redux/store';
+import HomeView from '../HomeView/HomeView';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { waitFor } from '@testing-library/react';
+import MovieView from './MovieView';
 
 
 describe('Movie view', () => {
-    it('Should render the movie view', async () => {
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                <MovieView />
-                </BrowserRouter>
-                </Provider>
-        )
-        
+  it('Should render app', async () => {
+      render(
+          <Provider store={store}>
+              <MemoryRouter initialEntries={["/Paketering-gruppexamination"]}>
+                  <Routes>
+                      <Route path="/Paketering-gruppexamination" element={<HomeView />}>
+                      </Route>
+                      <Route path="/Paketering-gruppexamination/movieview/" element={<MovieView />}>
+                      </Route>
+                  </Routes>
+              </MemoryRouter>
+          </Provider>
+      )
+      const input = await screen.findByRole('textbox')
+      await userEvent.type(input, 'forrest gump')
 
-    })
+      const movie = await screen.findByText('Forrest Gump')
+      await userEvent.click(movie)
+
+      await waitFor(() => {
+          expect(screen.getByText('1994', {exact: false})).toBeInTheDocument();
+      })
+
+      screen.debug()
+  })
 })
