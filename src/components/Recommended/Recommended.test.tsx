8066729479e5ba27b311ca4794dc538769800094
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2023-10-05 10:47:33 +0200

    Tester > trending, recommended. Uppd trending.tsx

---
+++ src/components/Recommended/Recommended.test.tsx
@@ -0,0 +1,48 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
+
+import HomeView from '../../views/HomeView/HomeView';
+import Recommended from './Recommended';
+import { BrowserRouter } from 'react-router-dom';
+
+// import userEvent from '@testing-library/user-event';
+
+describe.only('test that the base is being rendered', () => {
+    it('should display a heading with the text "Trending:" ', async () => {
+        render(
+            <Provider store={store}>
+            <BrowserRouter>
+              <HomeView />
+            </BrowserRouter>
+          </Provider>
+        )
+        const trendingHeading = await screen.findByText('Trending:');
+        expect(trendingHeading).toBeInTheDocument()
+    })
+    it('should display 2 arrow-buttons for the user to scroll-navigate in the carousel', () => {
+        render(
+            <Provider store={store}>
+            <BrowserRouter>
+              <Recommended />
+            </BrowserRouter>
+          </Provider>
+        )
+        const arrowButtons = screen.getAllByText('chevron', {exact: false})
+        expect(arrowButtons).toHaveLength(2)
+    })
+    it('should display 8 image-elements', () => {
+        render(
+            <Provider store={store}>
+            <BrowserRouter>
+              <Recommended />
+            </BrowserRouter>
+          </Provider>
+        )
+        const imageElements = screen.getAllByRole('img')
+        expect(imageElements).toHaveLength(23);
+    })
+})
