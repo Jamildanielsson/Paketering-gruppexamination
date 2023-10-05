7f8632cff527372225767c93392d166b4d49ae15
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-05 12:52:27 +0200

    skapat utils funktion för favorit tryck

---
+++ src/utils/favoriteClickHandler.ts
@@ -0,0 +1,19 @@
+import { addToFavorites, removeFromFavorites } from '../redux/favoritesSlice';
+
+/**
+ *	Checks if your movie you click on is favorite and remove or add is depending on that.
+ * @param movieTitle  The movie title we click on.
+ * @param favorites The favorites array from redux
+ * @param dispatch The dispatch function from redux
+ */
+export function favoriteClickHandler(
+  movieTitle: string,
+  favorites: string[],
+  dispatch: Function
+) {
+  if (favorites.includes(movieTitle)) {
+    dispatch(removeFromFavorites(movieTitle));
+  } else {
+    dispatch(addToFavorites(movieTitle));
+  }
+}
