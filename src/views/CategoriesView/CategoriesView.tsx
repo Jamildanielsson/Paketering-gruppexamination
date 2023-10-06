3180f0d69189a5f31e984ba7a00c5cafbd19d53e
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-06 17:45:00 +0200

    skapat funktion för att mappa ut karuseller

--- src/views/CategoriesView/CategoriesView.tsx
+++ src/views/CategoriesView/CategoriesView.tsx
@@ -2,15 +2,21 @@ import Header from '../../components/Header/Header';
 import './CategoriesView.scss';
 import allMoviesArray from '../../assets/movies.json';
 import { getUniqueGenres } from '../../utils/getUniqueGenres';
+import GenreCarousel from '../../GenreCarousel/GenreCarousel';
 
 function CategoriesView() {
   const availableGenres = getUniqueGenres(allMoviesArray);
 
   console.log(availableGenres);
 
+  const genreCarouselComponents = availableGenres.map((availableGenre) => (
+    <GenreCarousel key={availableGenre} genre={availableGenre} />
+  ));
+
   return (
     <div>
       <Header />
+      {genreCarouselComponents}
     </div>
   );
 }
