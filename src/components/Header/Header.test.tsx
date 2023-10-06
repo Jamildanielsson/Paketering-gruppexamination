5cfc80c0f15091eb5e2d6dda099a2fc6e4f39c4e
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-06 10:45:41 +0200

    uppdaterat tester så dom nu fungerar.

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,18 +1,32 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import Header from './Header';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
 
 describe(Header, () => {
   it('Should render the fakeflix logo in the header', () => {
-    render(<Header />, { wrapper: BrowserRouter });
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </Provider>
+    );
     const fakeFlixLogo = screen.getByAltText('Fakeflix logo');
 
     expect(fakeFlixLogo).toBeInTheDocument();
   });
 
   it('Should render the hamburger menu in the header', () => {
-    render(<Header />, { wrapper: BrowserRouter });
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </Provider>
+    );
     const hamburger = screen.getByRole('navigation');
 
     expect(hamburger).toBeInTheDocument();
