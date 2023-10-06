194010920ad9219c58c8b32ff21c85212fb2d1c8
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-06 11:54:42 +0200

    Implementerat funktion för att stänga naven när man trycker utanför

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useRef } from 'react';
 import './Hamburger.scss';
 
 function Hamburger({ isOpen, setIsOpen }: IHamburgerProps) {
+  const hamburgerRef = useRef<HTMLDivElement>(null);
+  window.addEventListener('click', handleClickOutsideNav);
+
+  function handleClickOutsideNav(event: MouseEvent) {
+    if (isOpen && !hamburgerRef.current?.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  }
+
   return (
     <section
+      ref={hamburgerRef}
       className={`hamburger-menu ${isOpen ? 'hamburger-menu--open' : ''}`}
       onClick={() => setIsOpen((prev) => !prev)}
     >
