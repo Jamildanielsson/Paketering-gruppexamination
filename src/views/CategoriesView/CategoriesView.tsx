5087abf5815d40e1b51291f126894bf3ea6a2e54
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-06 15:13:55 +0200

    gjort funktion för att hämta unika genrer

--- src/views/CategoriesView/CategoriesView.tsx
+++ src/views/CategoriesView/CategoriesView.tsx
@@ -1,11 +1,16 @@
 import Header from '../../components/Header/Header';
 import './CategoriesView.scss';
+import allMoviesArray from '../../assets/movies.json';
+import { getUniqueGenres } from '../../utils/getUniqueGenres';
 
 function CategoriesView() {
+  const availableGenres = getUniqueGenres(allMoviesArray);
+
+  console.log(availableGenres);
+
   return (
     <div>
       <Header />
-      Categories
     </div>
   );
 }
