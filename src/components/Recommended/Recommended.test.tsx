6f8ad6bad40c8a12b78cb93db52ab9a4e29c6dbf
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-09 23:16:58 +0200

    uppdaterat tester så dom nu funkar

--- src/components/Recommended/Recommended.test.tsx
+++ src/components/Recommended/Recommended.test.tsx
@@ -3,23 +3,10 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import store from '../../redux/store';
-import HomeView from '../../views/HomeView/HomeView';
 import Recommended from './Recommended';
 import { BrowserRouter } from 'react-router-dom';
 
 describe(Recommended, () => {
-  it('should display a heading with the text "Trending:" ', async () => {
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <HomeView />
-        </BrowserRouter>
-      </Provider>
-    );
-    const trendingHeading = await screen.findByText('Trending:');
-    expect(trendingHeading).toBeInTheDocument();
-  });
-
   it('should display 8 image-elements', () => {
     render(
       <Provider store={store}>
--- src/components/Trending/Trending.test.tsx
+++ src/components/Trending/Trending.test.tsx
@@ -3,29 +3,16 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import store from '../../redux/store';
-import HomeView from '../../views/HomeView/HomeView';
 import Trending from './Trending';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
-describe('test that the base is being rendered', () => {
-  it('should display a heading with the text "Trending:" ', async () => {
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <HomeView />
-        </BrowserRouter>
-      </Provider>
-    );
-    const trendingHeading = await screen.findByText('Trending:');
-    expect(trendingHeading).toBeInTheDocument();
-  });
-
+describe(Trending, () => {
   it('should display 8 image-elements', () => {
     render(
       <Provider store={store}>
-        <BrowserRouter>
+        <MemoryRouter>
           <Trending />
-        </BrowserRouter>
+        </MemoryRouter>
       </Provider>
     );
     const imageElements = screen.getAllByRole('img');
