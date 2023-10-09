199c83dfd6affa4a90cf97d548bffedf62ed4b73
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-10 00:16:21 +0200

    lagt till felhantering och bättre kommentar

--- src/utils/getUniqueGenres.ts
+++ src/utils/getUniqueGenres.ts
@@ -1,12 +1,18 @@
 /**
- *
+ * Takes an array of MovieData objects, extracts and trims the genres from each movie,
+ * removes any duplicates, sorts them alphabetically, and returns the resulting array.
  * @param arrayFromJson The Json array of movies
- * @returns Alphabetic-Sorted Unique Genres
+ * @returns An alphabetically sorted array of unique genres
  */
 export function getUniqueGenres(arrayFromJson: MovieData[]) {
   const uniqueGenres: string[] = [];
 
   for (const movie of arrayFromJson) {
+    if (typeof movie.genre !== 'string') {
+      console.error(`Invalid genre data: ${movie.genre}`);
+      continue;
+    }
+
     const genres = movie.genre.split(',').map((category) => category.trim());
 
     for (const genre of genres) {
