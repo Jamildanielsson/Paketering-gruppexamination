e75e90d2dd9fb937467396687b1e38fb8598d275
Dennis Byberg <dennis.byberg@hotmail.com>
2023-10-10 00:26:26 +0200

    ts förbättring

--- src/utils/favoriteClickHandler.ts
+++ src/utils/favoriteClickHandler.ts
@@ -2,15 +2,17 @@ import { addToFavorites, removeFromFavorites } from '../redux/favoritesSlice';
 import { Dispatch } from 'redux';
 
 /**
- *	Checks if your movie you click on is favorite and remove or add is depending on that.
- * @param movieTitle  The movie title we click on.
+ *	Checks if your movie you click on is favorite and remove or add is depending on that
+ * @param movieTitle  The movie title we click on
  * @param favorites The favorites array from redux
  * @param dispatch The dispatch function from redux
  */
 export function favoriteClickHandler(
   movieTitle: string,
   favorites: string[],
-  dispatch: Dispatch
+  dispatch: Dispatch<
+    ReturnType<typeof addToFavorites | typeof removeFromFavorites>
+  >
 ) {
   if (favorites.includes(movieTitle)) {
     dispatch(removeFromFavorites(movieTitle));
